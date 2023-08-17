4247926a51a2194151ca3d1f69b0c9c958626e32
Shobhit <shobhit.dubey@liseinfotech.com>
2023-08-17 18:33:26 +0530

    Initial Commit

---
+++ Example.cs
@@ -0,0 +1,207 @@
+﻿
+
+
+
+
+
+using Microsoft.Playwright;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using NUnit.Allure.Core;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.JavaScript;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace newplaywright
+{
+    [AllureNUnit]  
+    internal class Example
+    {
+       
+
+        [Test]
+        [Category("crud")]
+        public async Task test1() {
+            var playwright = await Playwright.CreateAsync();
+            var request = await playwright.APIRequest.NewContextAsync(new APIRequestNewContextOptions()
+            {
+                BaseURL = "https://reqres.in/"
+
+            }) ;
+
+           var response = await request.GetAsync("api/users?page=2");
+            var data=await response.TextAsync();
+            Console.WriteLine(data);
+            
+        
+        }
+
+        [Test]
+        public async Task  test2() { 
+           var playwright=await Playwright.CreateAsync();
+            var request = await playwright.APIRequest.NewContextAsync(new APIRequestNewContextOptions()
+            {
+                BaseURL = "https://reqres.in/"
+
+            });
+
+            var response = await request.PostAsync("api/users",new APIRequestContextOptions()
+            {
+              DataObject = new
+              {
+                  name = "morpheus",
+                  job = "leader"
+              }
+
+            });
+
+
+            var data = await response.TextAsync();
+            Console.WriteLine(data);
+
+        
+        }
+
+        [Test]
+        public async Task test3() { 
+        
+            var playwright=await Playwright.CreateAsync();
+            var request = await playwright.APIRequest.NewContextAsync(new APIRequestNewContextOptions(){
+                BaseURL = "https://reqres.in/"
+            });
+
+            var response = await request.DeleteAsync("api/users/2");
+            var data =await response.TextAsync();
+            Console.WriteLine($"response: {response.Status}");
+        }
+
+        [Test]
+        public async Task test4() { 
+        
+        var playwright=await Playwright.CreateAsync();
+            var request = await playwright.APIRequest.NewContextAsync(new APIRequestNewContextOptions()
+            {
+                BaseURL = "https://reqres.in/"
+            });
+
+            var response = await request.GetAsync("api/users/2");
+            String data = await response.TextAsync();
+            Console.WriteLine($"Response Status: {response.Status}");
+           // var jsonobj = System.Text.Json.JsonDocument.Parse(data).RootElement;
+          //  dynamic obj=JsonConvert.DeserializeObject(data);
+          JObject obj=JObject.Parse(data);  
+          String email = (String)obj["first_name"];
+
+            // Assert.
+          Console.WriteLine(email);  
+           Console.WriteLine(data);
+
+        }
+
+        [Test] 
+        public async Task Post() {
+            var playwriter = await Playwright.CreateAsync();
+            var request = await playwriter.APIRequest.NewContextAsync(new APIRequestNewContextOptions()
+                { 
+           BaseURL= "https://petstore.swagger.io/"
+            });
+
+
+
+            var response = await request.GetAsync("v2/pet/findByStatus?status=available");
+
+            var data = await response.TextAsync();
+            
+            Console.WriteLine(data);
+            var headers = response.Headers;
+            foreach (var header in headers) {
+                Console.WriteLine($"Header {header.Key}={header.Value}");
+            
+            }
+            var cookies = response.Headers.Where(header => header.Key.Equals("Set-Cookie"));
+            foreach (var cookie in cookies)
+            {
+                Console.WriteLine($"Set-Cookie:{cookie.Value}");
+            }
+        }
+
+        [Test]
+        public async Task test7() { 
+        var playwright=await Playwright.CreateAsync();
+            var request = await playwright.APIRequest.NewContextAsync(new APIRequestNewContextOptions()
+            {
+                BaseURL = "https://reqres.in/"
+
+            }) ;
+
+        
+        }
+
+        [Test]
+        public async Task test5()
+        {
+
+            var playwright = await Playwright.CreateAsync();
+            var request = await playwright.APIRequest.NewContextAsync(new APIRequestNewContextOptions()
+            {
+                BaseURL = "https://reqres.in/"
+            });
+
+            var response = await request.GetAsync("api/users");
+            String data = await response.TextAsync();
+            Console.WriteLine(data);
+            Console.WriteLine($"Response Status: {response.Status}");
+            //  dynamic obj=JsonConvert.DeserializeObject(data);
+
+
+            JObject obj = JObject.Parse(data);
+            JArray dataArray = (JArray)obj["data"];
+
+            JObject user = (JObject)dataArray[0];
+            // Assert.
+            Console.WriteLine(user);
+
+            Console.WriteLine(user["id"]);
+
+            Assert.AreEqual(1, (int)user["id"], "values are not equale");
+            Assert.AreEqual("George", (String)user["first_name"],"name is not matching");
+            
+        }
+
+        [Test]
+        public async Task authTest() {
+            const String AuthToken = "a6fc195dd60e618c4f0d37e15ae429917d090fe68d9ca16fd847681cddc448fa";
+            var playwright = await Playwright.CreateAsync();
+            var request = await playwright.APIRequest.NewContextAsync(new APIRequestNewContextOptions()
+            {
+                BaseURL = "https://gorest.co.in/"
+            });
+
+            var headers = new Dictionary<String, String> {
+                ["Authorization"]=$"Bearer {AuthToken}"
+            };
+            var response = await request.PostAsync("/public/v2/users/2627/posts", new APIRequestContextOptions()
+            {
+                DataObject = new
+                {
+                    title = "Shobhit",
+                    body = "Automation"
+
+                },
+
+                Headers = headers
+            }) ;
+
+            Console.WriteLine(response.Status);
+
+
+        }
+
+
+    }
+}
---
+++ NewTest.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace newplaywright;
+[TestFixture]
+public class NewTest
+
+{
+    [Test]
+    [Category("A")]
+
+
+
+
+    public void TestA()
+    {
+        Console.WriteLine("Console");
+    }
+
+    [Test]
+    [Category("B")]
+    public void TestB()
+    {
+        Console.WriteLine("B");
+    }
+
+    [Test]
+    [Category("C")]
+    public void TestC()
+    {
+        Console.WriteLine("C");
+    }
+
+    [Test]
+    [Category("A")]
+    [Category("B")]
+    public void TestAB()
+    {
+        Console.WriteLine("A-B");
+    }
+
+    [Test]
+    [Category("A")]
+    [Category("C")]
+    public void TestAC()
+    {
+        Console.WriteLine("A-C");
+    }
+
+    [Test]
+    [Category("B")]
+    [Category("C")]
+    public void TestBC()
+    {
+        Console.WriteLine("B-C");
+    }
+
+    [Test]
+    [Category("A")]
+    [Category("B")]
+    [Category("C")]
+    public void TestABC()
+    {
+        Console.WriteLine("A-B-C");
+    }
+}
